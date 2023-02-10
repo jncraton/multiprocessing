cf169165722672f7d5401a800633c56f378b2d6c
Jon Craton <jncraton@gmail.com>
2023-02-10 10:49:54 -0500

    Update handout code to indicate child creation

--- sum.c
+++ sum.c
@@ -13,9 +13,14 @@ int main() {
     children[i] = fork();
     if (children[i] == -1) {
       printf("Error forking process %d\n", children[i]);
-    } else if (children[i]) {
-      printf("PID %d complete\n", children[i]);
-      exit(1);
+    } else if (children[i] == 0) {
+      // We are the child process because PID returned by fork is 0
+      sleep(1);
+      printf("Child %d complete\n", i);
+      exit(0);
+    } else {
+      // We are the parent because PID returned by for is positive
+      printf("Child %d (PID %d) created\n", i, children[i]);
     }
   }
 
